a755873eb93b2705a45cbddd2a511033647eea4e
Macharia <simon7chris777@gmail.com>
2024-05-28 20:44:00 +0300

    config.php changed for heroku purposes

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 <?php
-// Use environment variables for database credentials.
-$host = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
-$database = getenv('DB_NAME');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+define('DB_HOST', $url["host"]);
+define('DB_USER', $url["user"]);
+define('DB_PASS', $url["pass"]);
+define('DB_NAME', substr($url["path"], 1));
 
 // Establish database connection.
 try {
-    $dbh = new PDO("mysql:host=$host;dbname=$database", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
